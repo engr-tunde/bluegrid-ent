479aa9ae58e2ef06b066d349e957dcacf31fbd44
HapothecaryJosh <tommycity10@gmail.com>
2025-11-14 11:25:29 +0100

    Worked on the video section

--- src/component/homepage/CreateMusicSection.jsx
+++ src/component/homepage/CreateMusicSection.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { createMusicSectionData, image } from "../../data/homePage";
+import { PiPersonSimpleSwimFill } from "react-icons/pi";
+import { BiLabel } from "react-icons/bi";
+import { GiMusicalNotes } from "react-icons/gi";
+import { CiVideoOn } from "react-icons/ci";
+
+
+
 
 const CreateMusicSection = () => {
   const [current, setCurrent] = useState(0);
@@ -25,9 +32,9 @@ const CreateMusicSection = () => {
       <h1 className="font-Syne w-[15%] lg:w-[8%] flex items-center uppercase justify-center [writing-mode:vertical-lr] transform rotate-180 text-3xl tracking-wider font-extrabold border-l border-r border-gray-700">
         BLUEGRID ENTERTAINMENT
       </h1>
-      <div className="w-[85%] lg:w-[92%] flex flex-col lg:flex-row pt-20">
+      <div className="w-[85%] lg:w-[92%] flex flex-col lg:flex-row lg:pt-20">
         {/* Slider Section */}
-        <div className="lg:w-[50%] mx-2 lg:px-0 mt-5 flex flex-col lg:flex-row relative">
+        <div className="lg:w-[40%] mx-2 lg:px-0 mt-5 flex flex-col lg:flex-row relative">
           <div className="w-[100%] flex flex-col gap-3 px-2">
             <h2 className="mt-4 lg:mt-0 text-2xl lg:text-4xl lg:leading-13 font-bold mb-2 uppercase">
               {createMusicSectionData[current]?.title}
@@ -75,37 +82,41 @@ const CreateMusicSection = () => {
         </div>
 
         {/* right side: original static content restored */}
-        <div className="w-[100%] lg:w-[50%] flex flex-col gap-5 pt-5 px-2 lg:pl-2">
-          <div className="flex flex-col gap-6 lg:gap-0 lg:flex-row w-full">
-            <div className="flex flex-col lg:w-[55%] gap-4 lg:gap-3 px-2">
-              <h3 className="font-semibold text-[20px] lg:text-[24px]">
-                CREATE SIGNS SUBLIME
-              </h3>
-              <span className="text-[16px] leading-7">
-                Sublime has officially signed with Create Music Group, a move
-                spearheaded by Create’s Senior VP of Music, Operations and
-                Marketing — Chris Herche, marking a new chapter in Sublime’s
-                legacy. The band made a powerful return with the release of
-                their first entirely original single, “Ensenada,” which debuted
-                on July 18, 2025. The track blends the band's iconic ska sound
-                with reggae and punk and is accompanied by a music video.
-                Fronted by Jakob Nowell, the son of the late Bradley Nowell, and
-                backed by original members Bud Gaugh and Eric Wilson, the band
-                recently performed at Vans Warped Tour in Long Beach and hosted
-                a multi‑night Las Vegas takeover event titled “Fear, Loathing,
-                and Sublime in Las Vegas.” Sublime’s upcoming appearances
-                include Sea.Hear.Now Music Festival and The Today Show both
-                happening in September 2025.
-              </span>
-            </div>
-            <Link
-              to="/"
-              className="flex lg:items-center lg:mx-auto text-[#008ad3] font-semibold opacity-90 hover:opacity-100"
-            >
-              <span className="read-more-link text-[20px] text-[#008ad3] font-semibold opacity-90 hover:opacity-100">
-                READ MORE
-              </span>
-            </Link>
+        <div className="w-[100%] lg:w-[60%] flex flex-col gap-5 pt-5 lg:pl-2 relative">
+          <div className="lg:w-[500px] lg:h-[500px] m-auto">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full h-full rounded-full"
+              src="/videos/bluegrid-vid-5.mp4"
+            ></video>
+          </div>
+
+          <div className="hidden absolute bg-primary-deep w-[150px] lg:flex flex-col rounded-lg p-3 left-20 top-35">
+            <span className="text-[16px]">For Artist</span>
+            <span className="text-secondary-brand text-[14px]">(Distributions)</span>
+
+            <div className="rounded-full flex items-center justify-center absolute top-0  bg-primary-light -right-7 h-full w-[40%] text-xl"><PiPersonSimpleSwimFill /></div>
+          </div>
+          <div className="hidden absolute bg-primary-deep w-[150px] lg:flex flex-col rounded-lg p-3 left-20 bottom-25">
+            <span className="text-[16px]">For Label</span>
+            <span className="text-secondary-brand text-[14px]">(Distributions)</span>
+
+            <div className="rounded-full flex items-center justify-center absolute top-0  bg-primary-light -right-7 h-full w-[40%] text-xl"><BiLabel /></div>
+          </div>
+           <div className="hidden absolute bg-primary-light w-[200px] lg:flex flex-col rounded-lg p-3 right-20 top-25">
+            <span className="text-[16px]">For White Labels</span>
+            <span className="text-secondary-brand text-[14px]">(Distributions)</span>
+
+            <div className="rounded-full flex items-center justify-center absolute top-0  bg-primary-deep -right-7 h-full w-[40%] text-xl"><GiMusicalNotes /></div>
+          </div>
+           <div className="hidden absolute bg-primary-light w-[150px] lg:flex flex-col rounded-lg p-3 right-20 bottom-35">
+            <span className="text-[16px]">For Videos</span>
+            <span className="text-secondary-brand text-[14px]">(Distributions)</span>
+
+            <div className="rounded-full flex items-center justify-center absolute top-0  bg-primary-deep -right-7 h-full w-[40%] text-xl"><CiVideoOn /></div>
           </div>
         </div>
       </div>
